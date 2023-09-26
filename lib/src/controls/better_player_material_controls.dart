0270445f2c61694646edcc99c45f18fe242f4350
Amr Saied <amr.saied@almentor.net>
2023-09-26 18:53:08 +0300

    enable pip

--- lib/src/controls/better_player_material_controls.dart
+++ lib/src/controls/better_player_material_controls.dart
@@ -216,50 +216,50 @@ class _BetterPlayerMaterialControlsState
     );
   }
 
-  // Widget _buildPipButton() {
-  //   return BetterPlayerMaterialClickableWidget(
-  //     onTap: () {
-  //       betterPlayerController!.enablePictureInPicture(
-  //           betterPlayerController!.betterPlayerGlobalKey!);
-  //     },
-  //     child: Padding(
-  //       padding: const EdgeInsets.all(8),
-  //       child: Icon(
-  //         betterPlayerControlsConfiguration.pipMenuIcon,
-  //         color: betterPlayerControlsConfiguration.iconsColor,
-  //       ),
-  //     ),
-  //   );
-  // }
+  Widget _buildPipButton() {
+    return BetterPlayerMaterialClickableWidget(
+      onTap: () {
+        betterPlayerController!.enablePictureInPicture(
+            betterPlayerController!.betterPlayerGlobalKey!);
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Icon(
+          betterPlayerControlsConfiguration.pipMenuIcon,
+          color: betterPlayerControlsConfiguration.iconsColor,
+        ),
+      ),
+    );
+  }
 
-  // Widget _buildPipButtonWrapperWidget(
-  //     bool hideStuff, void Function() onPlayerHide) {
-  //   return FutureBuilder<bool>(
-  //     future: betterPlayerController!.isPictureInPictureSupported(),
-  //     builder: (context, snapshot) {
-  //       final bool isPipSupported = snapshot.data ?? false;
-  //       if (isPipSupported &&
-  //           _betterPlayerController!.betterPlayerGlobalKey != null) {
-  //         return AnimatedOpacity(
-  //           opacity: hideStuff ? 0.0 : 1.0,
-  //           duration: betterPlayerControlsConfiguration.controlsHideTime,
-  //           onEnd: onPlayerHide,
-  //           child: Container(
-  //             height: betterPlayerControlsConfiguration.controlBarHeight,
-  //             child: Row(
-  //               mainAxisAlignment: MainAxisAlignment.end,
-  //               children: [
-  //                 _buildPipButton(),
-  //               ],
-  //             ),
-  //           ),
-  //         );
-  //       } else {
-  //         return const SizedBox();
-  //       }
-  //     },
-  //   );
-  // }
+  Widget _buildPipButtonWrapperWidget(
+      bool hideStuff, void Function() onPlayerHide) {
+    return FutureBuilder<bool>(
+      future: betterPlayerController!.isPictureInPictureSupported(),
+      builder: (context, snapshot) {
+        final bool isPipSupported = snapshot.data ?? false;
+        if (isPipSupported &&
+            _betterPlayerController!.betterPlayerGlobalKey != null) {
+          return AnimatedOpacity(
+            opacity: hideStuff ? 0.0 : 1.0,
+            duration: betterPlayerControlsConfiguration.controlsHideTime,
+            onEnd: onPlayerHide,
+            child: Container(
+              height: betterPlayerControlsConfiguration.controlBarHeight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  _buildPipButton(),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return const SizedBox();
+        }
+      },
+    );
+  }
 
   Widget _buildMoreButton() {
     return BetterPlayerMaterialClickableWidget(
@@ -453,7 +453,6 @@ class _BetterPlayerMaterialControlsState
     // final bool isFinished =
     return _buildHitAreaClickableButton(
       icon: isVideoFinished(_latestValue)
-      
           ? Icon(
               Icons.replay,
               size: 42,
@@ -664,7 +663,7 @@ class _BetterPlayerMaterialControlsState
 
   void _onPlayPause() {
     bool isFinished = false;
-    
+
     if (_latestValue?.position != null && _latestValue?.duration != null) {
       isFinished = _latestValue!.position >= _latestValue!.duration!;
     }
